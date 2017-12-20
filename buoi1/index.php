6c772b4cedc2032ac4eef934f9059bc9fe059ff1
Huong <you@example.com>
2017-12-20 18:02:48 +0700

    2

--- buoi1/index.php
+++ buoi1/index.php
@@ -1,5 +1,22 @@
 <?php
-echo "Hello World!";
+//echo "Hello World!";
+
+// $bool = true;
+// $int = 12; 
+//echo $bool;
+//print_r($int);
+//echo gettype($bool);
+
+//var_dump($int);
+
+// $str = 'Hello World!';
+// echo $str;
+/**
+ *  
+ * 
+ * 
+ * 
+ */
 
 ?>
 
