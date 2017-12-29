48b4e2665cd6bc55ca6edcfd24dd01cd4a87ce08
Huong <you@example.com>
2017-12-29 17:58:34 +0700

    fucntion

---
+++ buoi4/index.php
@@ -0,0 +1,52 @@
+<?php
+
+// function inSo(){
+//     $num = 10;
+//     echo $num;
+// }
+
+// function inSo(){
+//     $num = 10;
+//     return $num;
+// }
+
+// echo inSo();
+
+// function inSo($num){
+//     echo 2345667;
+//     echo "<br>";
+//     return $num;
+
+//     echo "KPT";
+// }
+
+// echo inSo(10);
+
+// die;
+
+function inSo($number){
+    $tmp = '';
+    for($i=0;$i<=$number; $i++){
+        $tmp.=$i.' ';
+    }
+    return $tmp;
+}
+
+echo inSo(10);
+
+/**
+ 
+
+*
+**
+***
+****
+*****
+****** 
+*******
+
+
+
+ */
+
+?>
