893d0b8f7c7b15087f6dbc613db04482eef2a567
Huong <you@example.com>
2018-01-03 18:41:48 +0700

    uploadmultifile

--- buoi6/form.php
+++ buoi6/form.php
@@ -7,8 +7,8 @@
     <title>Upload File</title>
 </head>
 <body>
-    <form action="xuly.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="my-file">
+    <form action="xuly-multiple.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="my-file" multiple>
         <button type="submit">Send</button>
     </form>
 </body>
---
+++ buoi6/xuly-multiple.php
@@ -0,0 +1,12 @@
+<?php
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+//file size < 1MB
+// 1MB = 1014 kB
+// 1kB = 1024 byte 
+
+// 84771 byte
+
+//chỉ cho phép upload file doc, docx, pdf , xlsx, rtf
+
+//rename
+
