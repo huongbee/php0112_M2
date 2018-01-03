fa35528f4240ec4771ffd4fd8be40612086a79c4
Huong <you@example.com>
2018-01-03 18:33:40 +0700

    uploadfile

--- buoi6/xuly.php
+++ buoi6/xuly.php
@@ -1,27 +1,52 @@
 <?php
-
+date_default_timezone_set('Asia/Ho_Chi_Minh');
 //file size < 1MB
 // 1MB = 1014 kB
 // 1kB = 1024 byte 
 
 // 84771 byte
 
+//chỉ cho phép upload file doc, docx, pdf , xlsx, rtf
 
-
-//chỉ cho phép upload file doc, docx, pdf
 //rename 
-if($_FILES['my-file']['name']!='' && $_FILES['my-file']['error'] == 0){
+
+$error = '';
+if(isset($_FILES['my-file']['name']) && $_FILES['my-file']['error'] == 0){
 
     $file = $_FILES['my-file'];
-    if(){
-        
+    
+    $size = $file['size']; //byte
+    if($size < 1024*1024){
+        //thoả mãn
+        //print_r($file);
+        $name = $file['name'];
+        $ext = strtolower(pathinfo($name,PATHINFO_EXTENSION));
+        $arrExt = ['doc', 'docx', 'pdf' , 'xlsx', 'rtf'];
+
+        if(in_array($ext,$arrExt)){
+            //cho phep
+            //a.doc
+            $newName = date('Y-m-d-H-i-s').'-'.$name;
+            move_uploaded_file($file['tmp_name'],"uploads/$newName");
+            echo "Upload successfully";
+        }
+        else{
+            $error =  "File không được phép chọn";
+        }
+
     }
-    //print_r($file);
+    else{
+        $error =  "File quá lớn, vui lòng chọn file có kích thước < 1MB";
+    }
+    
 }
 else{
-    echo "Bạn chưa chọn file";
-    echo "<br><a href='form.php'>Trở về</a>";
+    $error = "Bạn chưa chọn file";
 }
 
+if($error != ''){
+    echo $error;
+    echo "<br><a href='form.php'>Trở về</a>";
+}
 
 ?>
