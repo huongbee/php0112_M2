75946ff34880782e215930813ea61eee8a7a540a
Huong <you@example.com>
2018-01-03 19:07:03 +0700

    uploadmultifile

--- buoi6/xuly-multiple.php
+++ buoi6/xuly-multiple.php
@@ -11,7 +11,40 @@ date_default_timezone_set('Asia/Ho_Chi_Minh');
 
 $file = $_FILES['myfile'];
 
-echo "<pre>";
-print_r($file);
-echo '</pre>';
+$tmpName = $file['tmp_name'];
+$name = $file['name'];
+$size = $file['size'];
 
+foreach($size as $kichthuoc){
+    if($kichthuoc>=1014*1024){
+        //khong tm
+        echo "File too large";
+        die;
+    }
+}
+
+// echo "<pre>";
+// print_r($name);
+// echo '</pre>';
+
+// echo "<pre>";
+// print_r($tmpName);
+// echo '</pre>';
+
+$arrExt = ['doc', 'docx', 'pdf' , 'xlsx', 'rtf'];
+
+foreach($name as $key=>$file){
+    $ext = strtolower(pathinfo($file,PATHINFO_EXTENSION));
+    if(!in_array($ext,$arrExt)){
+        //ko cho phep
+        echo "File khong duoc phep chon";
+        die;
+    }
+}
+
+foreach($name as $key=>$file){
+    $newName = rand(13232,987654323).'-'.$file;
+    move_uploaded_file($tmpName[$key],'uploads/'.$newName);
+    
+}
+echo 'success';
