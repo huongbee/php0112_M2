30412b9ed5bddc40286badaf0854a354dd63e472
Huong <you@example.com>
2018-01-03 17:59:04 +0700

    uploadfile

--- buoi6/xuly.php
+++ buoi6/xuly.php
@@ -1,7 +1,27 @@
 <?php
 
-//file size <1MB
+//file size < 1MB
+// 1MB = 1014 kB
+// 1kB = 1024 byte 
+
+// 84771 byte
+
+
+
 //chỉ cho phép upload file doc, docx, pdf
 //rename 
+if($_FILES['my-file']['name']!='' && $_FILES['my-file']['error'] == 0){
+
+    $file = $_FILES['my-file'];
+    if(){
+        
+    }
+    //print_r($file);
+}
+else{
+    echo "Bạn chưa chọn file";
+    echo "<br><a href='form.php'>Trở về</a>";
+}
+
 
 ?>
