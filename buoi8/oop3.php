0e4c36ccc69e6ec10c2570e9921248e526604ca4
Huong <you@example.com>
2018-01-08 19:02:47 +0700

    oop3

--- buoi8/oop3.php
+++ buoi8/oop3.php
@@ -8,7 +8,7 @@ class SinhVien{
         //echo $data;
         echo "Construct:<br>Id cua sv la: ".$this->id;
         echo "<br>";
-        echo "Class ". get_class(). " vua khoi tao";
+        echo "Class ". get_class($this). " vua khoi tao";
     }
 
     function getA(){
@@ -16,25 +16,37 @@ class SinhVien{
         echo 2323;
     }
 
-    function __destruct(){
+    private function __destruct(){
         //unset($this->id); //huy bien
 
         echo "<hr>Destruct:<br>";
         echo  "Id cua sv la: ".$this->id;
         echo "<br>";
-        echo "Class ". get_class(). " vua bi huy";
+        echo "Class ". get_class($this). " vua bi huy";
     }
 
     function getB(){
         echo "<br>";
         echo "BBBB";
     }
+    function seeMore(){
+        echo get_class($this); //la obj duoc new
+        echo "<br>";
+        echo get_class(); //của chính  class chứa nó
+    }
 }
 
 class hocsinh extends SinhVien{
 
+    function __destruct(){
+        echo "<br>";
+        echo "Class ". get_class($this). " vua bi huy";
+    }
+    
 }
 $hs = new hocsinh(2323);
+echo "<hr>";
+$hs->seeMore();
 
 // $sv = new SinhVien(123);
 // $sv->getA();
