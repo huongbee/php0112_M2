eba968e65ce3562cfbca5a5de1681b6a5ee819d6
Huong <you@example.com>
2018-01-08 18:00:15 +0700

    oop

---
+++ buoi8/oop.php
@@ -0,0 +1,51 @@
+<?php
+
+class SinhVien{
+    public $id;
+    public $name;
+
+    function setId($mssv){
+        $this->id = $mssv;
+    }
+
+    function getId(){
+        return $this->id;
+    }
+
+    function setName($name){
+        $this->name = $name;
+    }
+    function getName(){
+        return $this->name;
+    }
+
+}
+
+$sv = new SinhVien;
+$sv->setId('SV123456');
+$sv->name = "Khoa Pham";
+//echo $sv->id;
+echo $sv->getId();
+echo "<br>";
+echo $sv->getName();
+
+//var_dump($sv);
+
+echo "<br>";
+$sv2 = new SinhVien;
+$sv2->setId('SV123');
+$sv2->setName("Ti");
+$sv2->age = 12; //set
+
+echo $sv2->getId();
+echo "<br>";
+echo $sv2->getName();
+echo "<br>";
+echo $sv2->age; //get
+
+echo "<br>";
+var_dump($sv2);
+
+
+
+?>
