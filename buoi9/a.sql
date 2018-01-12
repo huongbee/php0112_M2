5402f4b72a527f6d042a4b39cbc8d92106c65975
Huong <you@example.com>
2018-01-12 18:21:59 +0700

    sql

---
+++ buoi9/a.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE php0112
+
+DROP DATABASE php0112
+
+CREATE TABLE `users` ( 
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'khoa chinh' , 
+    `username` VARCHAR(20) NOT NULL DEFAULT 'admin' , 
+    `email` VARCHAR(50) NULL DEFAULT NULL COMMENT 'email...' , 
+    `fullname` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL , 
+    `password` VARCHAR(100) NULL DEFAULT NULL , 
+    `birthdate` DATE NULL DEFAULT NULL , 
+    `created_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+    PRIMARY KEY (`id`), 
+    UNIQUE `unique` (`email`)
+)
+
+CREATE TABLE bill(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    total DOUBLE DEFAULT 0,
+    date_order DATETIME NULL,
+    PRIMARY KEY (id)
+)
+
+
+
+ALTER TABLE `users` CHANGE `id` `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'khoa chinh';
+
+ALTER TABLE bill CHANGE date_order date_order  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+
+ALTER TABLE `bill` CHANGE `date_order` `date_order` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+ALTER TABLE bill
+ADD quantity INT(11) DEFAULT 0
+
+ALTER TABLE `users` ADD `gender` VARCHAR(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'nam' AFTER `fullname`;
+
+-- https://www.w3resource.com/mysql/mysql-tutorials.php
